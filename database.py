3b0ad3f904b6703a247e9a39efc5c39eac381a0f
GianMarcoZampa <gianmarco.zampa@gmail.com>
2019-11-11 16:14:41 +0100

    Add database management class with add, get, update and delete method

--- database.py
+++ database.py
@@ -1,3 +1,58 @@
-
 class DataBase:
-    pass
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.users = {}
+        self.load()
+
+    # This method is called by the constructor and loads stored data in users variable
+    def load(self):
+        with open(self.file_name, "r") as f:
+            lines = f.readlines()
+
+            for line in lines:
+                email, nickname, password = line.split(";", 3)
+                self.users[email] = [nickname, password]
+
+    # This method returns the user data if it already exists else false
+    def get(self, email):
+        if email in self.users:
+            return self.users[email]
+        else:
+            return False
+
+    # This method adds an user to database if it doesn't exists yet else flase
+    def add(self, email, nickname, password):
+        if email not in self.users:
+            self.users[email] = [nickname, password]
+            self.save()
+            return True
+        else:
+            print("Email already exists")
+            return False
+
+    # This method deletes an user from the database if it already exists else false
+    def delete(self, email):
+        if email in self.users:
+            self.users.pop(email)
+            self.save()
+            return True
+        else:
+            print("Cannot delete account")
+            return False
+
+    # This method updates user data if the user already exists else false
+    def update(self, email, nickname, password):
+        if email in self.users:
+            self.users[email] = [nickname, password]
+            self.save()
+            return True
+        else:
+            print("Email doesn't exists")
+            return False
+
+    # This method saves values stored in users inside a .txt file
+    def save(self):
+        with open(self.file_name, "w") as f:
+            for user in self.users.items():
+                line = user[0] + ";" + user[1] + ";" + user[3] + "\n"
+                f.write(line)
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import kivy
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 
-kv = Builder.load_file("my.kv")
+from database import DataBase
 
-
-class Widgets(Widget):
-    pass
+kv = Builder.load_file("draw.kv")
+sm = ScreenManager()
+db = DataBase("user.txt")
 
 
 class DrawApp(App):
