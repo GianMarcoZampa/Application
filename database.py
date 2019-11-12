22beb452d13ae1dc764bc97a3c725da9c0309139
GianMarcoZampa <gianmarco.zampa@gmail.com>
2019-11-12 10:24:14 +0100

    Add login system and invalid data handler

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+
 class DataBase:
     def __init__(self, file_name):
         self.file_name = file_name
--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-import kivy
+
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
 
 from database import DataBase
+from windows import LoginWindow, RegisterWindow, MainWindow
 
 kv = Builder.load_file("draw.kv")
 sm = ScreenManager()
 db = DataBase("user.txt")
 
+screens = [LoginWindow(name="login", database=db, screen_manager=sm), RegisterWindow(name="register"), MainWindow(name="main")]
+for screen in screens:
+    sm.add_widget(screen)
+sm.current = "login"
+
 
 class DrawApp(App):
     def build(self):
-        return kv
+        return sm
+
+
+if __name__ == "__main__":
+    DrawApp().run()
